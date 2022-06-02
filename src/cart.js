c097bd203cebfe9beaca6487253df2ef2c89f631
Azhar Beg <99490876+azhar-beg@users.noreply.github.com>
2022-06-02 10:12:44 +0530

    Made class properties private
    - Modified `remove` method

--- src/cart.js
+++ src/cart.js
@@ -1,24 +1,28 @@
 class Cart {
+  #items;
   constructor() {
-    this.items = [];
+    this.#items = [];
   }
 
   equals(anotherCart) {
     return anotherCart instanceof Cart &&
-      this.items.length === anotherCart.items.length &&
-      this.items.every((item, index) => item.equals(anotherCart.items[index]));
+      this.#items.length === anotherCart.#items.length &&
+      this.#items.every((item, index) =>
+        item.equals(anotherCart.#items[index]));
   }
 
   add(item) {
-    this.items.push(item);
+    this.#items.push(item);
   }
 
   remove(id) {
-    this.items = this.items.filter((item) => !item.isOf(id));
+    const itemToRemove = this.#items.find((item) => item.isOf(id));
+    const index = this.#items.indexOf(itemToRemove);
+    this.#items.splice(index, 1);
   }
 
   totalPrice() {
-    return this.items.reduce((total, item) => total + item.getPrice(), 0);
+    return this.#items.reduce((total, item) => total + item.getPrice(), 0);
   }
 
 }
--- test/testCart.js
+++ test/testCart.js
@@ -8,10 +8,10 @@ describe('Cart', () => {
     const item2 = new Item(2, 'watch', 100);
 
     const cart1 = new Cart();
-    cart1.items.push(item1, item2);
+    cart1.add(item1, item2);
 
     const cart2 = new Cart();
-    cart2.items.push(item1, item2);
+    cart2.add(item1, item2);
 
     assert.ok(cart1.equals(cart2));
   });
@@ -21,38 +21,27 @@ describe('Cart', () => {
     const item2 = new Item(2, 'watch', 100);
 
     const cart1 = new Cart();
-    cart1.items.push(item1, item2);
+    cart1.add(item1);
+    cart1.add(item2);
 
     const cart2 = new Cart();
-    cart2.items.push(item1);
+    cart2.add(item1);
     assert.strictEqual(cart1.equals(cart2), false);
 
-    cart2.items.push(item1);
+    cart2.add(item1);
     assert.strictEqual(cart1.equals(cart2), false);
-  });
-
-  it('should add given item in cart', () => {
-    const item = new Item(1, 'watch', 100);
-
-    const actualCart = new Cart();
-    actualCart.items.push(item);
-
-    const expectedCart = new Cart();
-    expectedCart.add(item);
-
-    assert.ok(actualCart.equals(expectedCart));
   });
 
   it('should return total price of the items in the cart', () => {
     const item = new Item(1, 'watch', 100);
 
     const cart = new Cart();
-    cart.items.push(item);
+    cart.add(item);
 
     assert.deepStrictEqual(cart.totalPrice(), 100);
 
     const item2 = new Item(2, 'watch', 500);
-    cart.items.push(item2);
+    cart.add(item2);
     assert.deepStrictEqual(cart.totalPrice(), 600);
   });
 
@@ -60,7 +49,7 @@ describe('Cart', () => {
     const item = new Item(1, 'watch', 100);
 
     const actualCart = new Cart();
-    actualCart.items.push(item);
+    actualCart.add(item);
     actualCart.remove(1);
 
     const expectedCart = new Cart();
