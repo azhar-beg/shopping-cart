de32a8e3dca5fb828ad6de0b5a163553f9af5c78
Azhar Beg <99490876+azhar-beg@users.noreply.github.com>
2022-06-02 11:35:42 +0530

    Removed only from tests

--- test/testCartItem.js
+++ test/testCartItem.js
@@ -1,10 +1,16 @@
 const assert = require('assert');
 const { CartItem } = require('../src/cartItem.js');
 
-describe.only('CartItem', () => {
+describe('CartItem', () => {
   it('Should check equality of two items', () => {
     const item1 = new CartItem(1, 'watch', 100, 1);
     const item2 = new CartItem(1, 'watch', 100, 1);
     assert.ok(item1.equals(item2));
+  });
+
+  it('Should check equality of two unequal items', () => {
+    const item1 = new CartItem(1, 'watch', 10, 1);
+    const item2 = new CartItem(1, 'watch', 100, 1);
+    assert.strictEqual(item1.equals(item2), false);
   });
 });
